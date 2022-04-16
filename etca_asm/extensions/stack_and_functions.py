d36e91fca4acb1ca50bdd85e683fddfe1061655e
JKTKops <zerglingk9012@gmail.com>
2022-04-15 19:36:55 -0500

    Attempt to implement stack+functions extension

---
+++ etca_asm/extensions/stack_and_functions.py
@@ -0,0 +1,90 @@
+from wsgiref import validate
+from etca_asm.core import Extension, reject
+from etca_asm.base_isa import CONDITION_NAMES, validate_registers, build
+
+functions = Extension(2, "functions", "Stack and Functions")
+Register = tuple[int | None, int]
+
+def oneof(*names):
+    names=sorted(names, key=len, reverse=True)
+    return f"({'|'.join(names)})"
+
+REGISTERS = {
+    "a0": 0,
+    "a1": 1,
+    "v0": 2,
+    "s0": 3,
+    "s1": 4,
+    "bp": 5,
+    "sp": 6,
+    "ln": 7,
+}
+
+@functions.reg(fr'"%" /(bp|sp|ln)/ size_postfix')
+@functions.reg(fr'/(bp|sp|ln)/ size_postfix', prefix=False)
+def fn_ptr_registers(cxt, name, size):
+    return size, REGISTERS[name]
+
+@functions.reg(fr'"%" /(a|v|s)/ size_infix /(?!<\s)[0-1]/')
+#@functions.reg(fr'/(a|v|s)/ size_infix /[0-1]/', prefix=False)
+@functions.reg(fr'/(a|v|s)/ size_infix /(?!<\s)[0-1]/', prefix=False)
+def fn_gp_registers(cxt, pref, size, suff):
+    name = pref + suff
+    reject(not name in REGISTERS.keys(), f"Unknown register name `{name}'")
+    return size, REGISTERS[name]
+
+@functions.inst(f'"pop" size_postfix register')
+def pop_inst(cxt, inst_size, reg: Register):
+    size, (dst,) = validate_registers(cxt, reg, inst_size=inst_size)
+    return build((0b00,2), (cxt.register_sizes[size],2), (0xC,4), (dst,3), (0b00000,5))
+
+@functions.inst(f'"push" size_postfix register')
+def push_register_inst(cxt, inst_size, reg: Register):
+    size, (src,) = validate_registers(cxt, reg, inst_size=inst_size)
+    return build((0b00,2), (cxt.register_sizes[size],2), (0xD,4), (0b000,3), (src,3), (0b00,2))
+
+@functions.inst(f'"push" size_postfix immediate')
+def push_register_imm(cxt, inst_size, imm: int):
+    size, () = validate_registers(cxt, inst_size=inst_size)
+    reject(
+        not isinstance(imm, int) or not (0 <= imm < 32),
+        f"Invalidate immediate {imm} for op `push'"
+    )
+    return build((0b01,2), (cxt.register_sizes[size],2), (0xD,4), (0b000,3), (imm,5))
+
+@functions.inst(f'/j{oneof(*CONDITION_NAMES)}/ register')
+def cond_abs_reg_jump(cxt, inst: str, reg: Register):
+    _,(src,) = validate_registers(cxt, reg)
+    cc = inst.removeprefix('j')
+    op = CONDITION_NAMES[cc]
+    return build((0xAF,8), (src,3), (0b0,1), (op,4))
+
+@functions.inst(f'/ret{oneof(*CONDITION_NAMES)}/')
+def cond_return(cxt, inst: str):
+    cc = inst.removeprefix('ret')
+    reject(cc == 'mp', "`mp' is not a valid conditional return suffix")
+    op = CONDITION_NAMES[cc]
+    return build((0xAF,8), (0b111,3), (0b0,1), (op,4))
+
+@functions.inst(f'/call{oneof(*CONDITION_NAMES)}/ register')
+def cond_abs_reg_call(cxt, inst: str, reg: Register):
+    _,(src,) = validate_registers(cxt, reg)
+    cc = inst.removeprefix('call')
+    reject(cc == 'mp', "`mp' is not a valid conditional call suffix")
+    op = CONDITION_NAMES[cc]
+    return build((0xAF,8), (src,3), (0b1,1), (op,4))
+
+@functions.inst('"call" label')
+def abs_near_call(cxt, lbl: str):
+    target = cxt.resolve_label(lbl)
+    if target is None:
+        target = cxt.ip
+    bottom_mask = 0xfff
+    top_mask = ~bottom_mask
+    reject(
+        (top_mask & target) != (top_mask & cxt.ip),
+        f"""Cannot encode far call:
+    from `call {lbl}'     at 0x{cxt.ip:04x}
+    to   `{lbl}' resolved to 0x{target:04x}"""
+    )
+    return build((0xB,4), (bottom_mask & target, 12))
