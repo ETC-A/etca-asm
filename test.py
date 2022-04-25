aec5e470c9aa9be10b26aa383aa0ccf85f06634a
MegaIng <cornelius@krupp.hamburg>
2022-04-25 06:56:33 +0200

    mov-macro fixups

--- test.py
+++ test.py
@@ -10,8 +10,20 @@ etca_asm.extensions.import_all_extensions()
 ass = Assembler()
 
 res = ass.n_pass("""
-.set test '\\n'
-.dword test
+mov r0, -32768
+mov r0, -31000
+mov r0, -3100
+mov r0, -310
+mov r0, -31
+mov r0, -16
+mov r0, 0
+mov r0, 16
+mov r0, 17
+mov r0, 31
+mov r0, 310
+mov r0, 3100
+mov r0, 31000
+mov r0, 65535
 """)
 
 print(res.to_bytes().hex(' ', 2))
