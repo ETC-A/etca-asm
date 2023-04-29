f23bb6c4e62d4c704e6b99be9fe3cb08301ec722
JKTKops <zerglingk9012@gmail.com>
2023-04-29 01:10:21 -0500

    trial and error until I got it right

--- src/etc_as/extensions/dword_pointers.py
+++ src/etc_as/extensions/dword_pointers.py
@@ -1,8 +1,8 @@
 from etc_as.core import Extension
-from etc_as.base_isa import NAMED_CRS
 
-dword_ptrs = Extension(16, "dword_pointers", "32 Bit Address Space")
+dword_ptrs = Extension(16, "real32", "32 Bit Address Space")
 
-@dword_ptrs.set_init
-def dword_ptrs_init(context):
-    NAMED_CRS['mode'] = 17
+@dword_ptrs.register_syntax("control_register", f'"mode"', prefix=False)
+@dword_ptrs.register_syntax("control_register", f'"%mode"', prefix=True)
+def named_cr(context):
+    return 17
