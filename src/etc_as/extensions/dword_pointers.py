6d12e85e9878379223d3ce9348d9f8ae40fbf355
JKTKops <zerglingk9012@gmail.com>
2023-04-29 00:56:12 -0500

    init has to take context argument

--- src/etc_as/extensions/dword_pointers.py
+++ src/etc_as/extensions/dword_pointers.py
@@ -4,5 +4,5 @@ from etc_as.base_isa import NAMED_CRS
 dword_ptrs = Extension(16, "dword_pointers", "32 Bit Address Space")
 
 @dword_ptrs.set_init
-def dword_ptrs_init():
+def dword_ptrs_init(context):
     NAMED_CRS['mode'] = 17
